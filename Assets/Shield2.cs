b8acc71ca7d824219c02f01c943819254f149709
yevagami <antonius.adriel@gmail.com>
2023-12-07 13:07:35 -0500

    Fixed shield 2

--- Assets/Shield2.cs
+++ Assets/Shield2.cs
@@ -7,43 +7,62 @@ public class Shield2 : MonoBehaviour
     public GameObject shieldObject;
 
     private bool activeShield;
+    private bool cooldown;
 
     private float shieldDuration = 5.0f;
     private float shieldCooldown = 10.0f;
 
     private float shieldTimer;
-
+    private float shieldCooldownTimer;
 
     void Start()
     {
         activeShield = false;
+        cooldown = false;
         shieldObject.SetActive(false);
         shieldTimer = 0.0f;
+        shieldCooldownTimer = 0.0f;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift) && !activeShield && shieldTimer <= 0.0f){
-            shieldObject.SetActive(true);
-            activeShield = true;
-            shieldTimer = shieldDuration;
-            Debug.Log("shield active, (active for 5s)");
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !cooldown){
+            if (!activeShield)
+            {
+                shieldObject.SetActive(true);
+                activeShield = true;
+                shieldTimer = 0;
+            }
+            else
+            {
+                shieldObject.SetActive(false);
+                activeShield = false;
+                shieldTimer = 0;
+                cooldown = true;
+            }
         }
 
         if(activeShield){
+            shieldTimer += Time.deltaTime;
+            Debug.Log("shield active, (active for " + Mathf.Round((shieldDuration - shieldTimer)) + ")");
 
-            shieldTimer -= Time.deltaTime;
-
-            if(shieldTimer <= 0.0f){
+            if (shieldTimer >= shieldDuration){
                 shieldObject.SetActive(false);
                 activeShield = false;
-                shieldTimer = shieldCooldown;
+                shieldTimer = 0.0f;
+                cooldown = true;
             }
         }
-        else if (shieldTimer > 0.0f){
-            Debug.Log("on cooldown (10s cooldown)");
+
+        if (cooldown){
+            shieldCooldownTimer += Time.deltaTime;
+            Debug.Log("On cooldown, (active for " + Mathf.Round((shieldCooldown - shieldCooldownTimer)) + ")");
+
+            if (shieldCooldownTimer >= shieldCooldown) {
+                cooldown = false;
+                shieldCooldownTimer = 0.0f;
+            }
         }
- 
     }
 
 
