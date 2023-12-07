6bbbbf31de2e9b7bcc66b51ac92099eda4bc728d
Delianomy <delianomy@gmail.com>
2023-12-06 21:52:39 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa

---
+++ Assets/Enemy/Steering.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering 
+{
+    // Start is called before the first frame update
+
+
+    // Keep values for rotation and forward movement
+    public float angular;
+    public Vector3 linear;
+
+    public Steering()
+    {
+
+        // Initialize the variables
+
+
+    }
+}
+
